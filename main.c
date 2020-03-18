d80103b823e996f5b857cd17f73da60acc4a1a19
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-03-18 20:39:59 +0000

    comments, add an api

--- main.c
+++ main.c
@@ -1,5 +1,60 @@
+/*
+ * Copyright © 2014 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including
+ * the next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Parse a pci.ids text file to extract 'device_name'
+ * # Vendors, devices and subsystems. Please keep sorted.
+ * # Syntax:
+ * # vendor  vendor_name
+ * #	device  device_name				<-- single tab
+ * #		subvendor subdevice  subsystem_name	<-- two tabs
+ */
+
+/*
+ * Example section of file. Searching for 1002/130c
+ *
+ * 1002  Advanced Micro Devices, Inc. [AMD/ATI]
+ * # fields elided
+ *	130a  Kaveri [Radeon R6 Graphics]
+ *	130b  Kaveri [Radeon R4 Graphics]
+ *
+ *	130c  Kaveri [Radeon R7 Graphics] <- result
+ * #          ^-------------------------^
+ *
+ *	130d  Kaveri [Radeon R6 Graphics]
+ *	130e  Kaveri [Radeon R5 Graphics]
+ * # fields elided
+ * # next vendor region starts
+ * 1003  ULSI Systems
+ *	0201  US201
+ */
+
 #define _GNU_SOURCE
 #include <string.h>
+
+#include "pci_ids.h"
 
 #include "EvilUnit.h"
 #include "parse.h"
@@ -16,15 +71,65 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-/*
- * Parse the pci.ids file
- * Syntax:
- * '#' starts a comment line
- * # vendor  vendor_name
- * #	device  device_name				<-- single tab
- * #		subvendor subdevice  subsystem_name	<-- two tabs
- * This parser seeks 'device_name' and ignores subvendor fields
- */
+static struct pci_ids pci_ids_create_from_file(const char *path) {
+  struct pci_ids failure = {
+      .fd = -1,
+  };
+
+  int fd = open(path, O_RDONLY, 0);
+  if (fd == -1) {
+    return failure;
+  }
+
+  struct stat sb;
+  fstat(fd, &sb);
+  size_t sz = sb.st_size;
+
+  if (sz == 0) {
+    close(fd);
+    return failure;
+  }
+  // No attempt to read files > 4gb in size
+  sz = (sz < UINT32_MAX) ? sz : UINT32_MAX;
+  void *addr = mmap(0, sz, PROT_READ, MAP_PRIVATE, fd, 0);
+
+  if (addr == MAP_FAILED) {
+    close(fd);
+    return failure;
+  }
+
+  return (struct pci_ids){
+      .fd = fd,
+      .addr = addr,
+      .size = sz,
+  };
+}
+
+struct pci_ids pci_ids_create(void) {
+  static const char *paths[] = {
+      "/usr/share/hwdata/pci.ids", // update-pciids
+      "/usr/share/misc/pci.ids",   // debian
+      "/usr/share/pci.ids",        // redhat
+      "/var/lib/pciutils/pci.ids", // also debian
+      "pci.ids",
+  };
+  size_t sz = sizeof(paths) / sizeof(paths[0]);
+  for (size_t i = 0; i < sz; i++) {
+    struct pci_ids res = pci_ids_create_from_file(paths[i]);
+    if (res.fd != -1) {
+      return res;
+    }
+  }
+
+  return (struct pci_ids){.fd = -1};
+}
+
+void pci_ids_destroy(struct pci_ids f) {
+  if (f.fd != -1) {
+    munmap(f.addr, f.size);
+    close(f.fd);
+  }
+}
 
 struct range {
   // Iterator pair, start <= end. Can dereference [start end)
@@ -33,7 +138,7 @@ struct range {
 };
 static bool empty_range(struct range r) { return r.start == r.end; }
 
-static void write_as_four_hex_chars(uint16_t x, char *b) {
+static void write_as_hex(uint16_t x, char *b) {
   const char digits[] = "0123456789abcdef";
   for (unsigned i = 0; i < 4; i++) {
     unsigned index = 0xf & (x >> 4 * (3 - i));
@@ -49,7 +154,7 @@ static struct range find_vendor(struct range r, uint16_t VendorId)
   }
 
   char needle[5] = "\n0000";
-  write_as_four_hex_chars(VendorId, &needle[1]);
+  write_as_hex(VendorId, &needle[1]);
   unsigned char *s = memmem(r.start, r.end - r.start, needle, sizeof(needle));
 
   if (s) {
@@ -60,21 +165,6 @@ static struct range find_vendor(struct range r, uint16_t VendorId)
   }
   return r;
 }
-
-/*
- *
- * 1002  Advanced Micro Devices, Inc. [AMD/ATI]
- *	1304  Kaveri
- *	1305  Kaveri
- * # comments...
- *	cbb2  RS200 Host Bridge
- * 1003  ULSI Systems
- *	0201  US201
- * 1004  VLSI Technology Inc
- * 	0005  82C592-FC1
- * 	0006  82C593-FC1
- *
- */
 
 static struct range find_device(struct range r, uint16_t DeviceId) {
   if (empty_range(r)) {
@@ -95,7 +185,7 @@ static struct range find_device(struct range r, uint16_t DeviceId) {
   assert(r.start[0] == '\n');
 
   char needle[6] = "\n\t0000";
-  write_as_four_hex_chars(DeviceId, &needle[2]);
+  write_as_hex(DeviceId, &needle[2]);
 
   for (;;) {
     size_t width = r.end - r.start;
@@ -139,28 +229,28 @@ fail:
   return (struct range){0, 0};
 }
 
-static void copy_range_to_buffer(struct range r, char *buf, int size) {
+static void copy_range_to_buffer(struct range r, char *buf, size_t size) {
   assert(!empty_range(r));
-  int to_copy = (int)(r.end - r.start);
+  size_t to_copy = (r.end - r.start);
   to_copy = to_copy < (size - 1) ? to_copy : size - 1;
 
   memcpy(buf, r.start, to_copy);
   buf[to_copy] = '\0';
 }
 
-__attribute__((used)) static void write_fallback_to_buffer(char *buf, int size,
-                                                           uint16_t DeviceId) {
+__attribute__((used)) static void
+write_fallback_to_buffer(char *buf, size_t size, uint16_t DeviceId) {
   char tmp[] = "Device xxxx";
   static_assert(sizeof(tmp) == 12, "");
-  write_as_four_hex_chars(DeviceId, &tmp[7]);
+  write_as_hex(DeviceId, &tmp[7]);
 
-  int to_copy = ((int)sizeof(tmp) <= size) ? sizeof(tmp) : size;
+  size_t to_copy = (sizeof(tmp) <= size) ? sizeof(tmp) : size;
   memcpy(buf, tmp, to_copy);
   buf[size - 1] = '\0';
 }
 
-static void fill_buffer_from_device_range(char *buf, int size, struct range r,
-                                          uint16_t DeviceId) {
+static void fill_buffer_from_device_range(char *buf, size_t size,
+                                          struct range r, uint16_t DeviceId) {
   if (empty_range(r)) {
     write_fallback_to_buffer(buf, size, DeviceId);
 
@@ -169,62 +259,8 @@ static void fill_buffer_from_device_range(char *buf, int size, struct range r,
   }
 }
 
-typedef struct {
-  int fd;
-  unsigned size;
-  void *addr;
-} mmapped_file_t;
-
-static_assert(sizeof(mmapped_file_t) == 16, "");
-static mmapped_file_t open_mmapped_file(void) {
-
-  const char *path = "pci.ids";
-  mmapped_file_t failure = {
-      .fd = -1,
-  };
-
-  int fd = open(path, O_RDONLY, 0);
-  if (fd == -1) {
-    return failure;
-  }
-
-  struct stat sb;
-  fstat(fd, &sb);
-  size_t sz = sb.st_size;
-
-  if (sz == 0) {
-    close(fd);
-    return failure;
-  }
-  // No attempt to read files > 4gb in size
-  sz = (sz < UINT32_MAX) ? sz : UINT32_MAX;
-  void *addr = mmap(0, sz, PROT_READ, MAP_PRIVATE, fd, 0);
-
-  if (addr == MAP_FAILED) {
-    close(fd);
-    return failure;
-  }
-
-  return (mmapped_file_t){
-      .fd = fd,
-      .addr = addr,
-      .size = sz,
-  };
-}
-
-static void close_mmapped_file(mmapped_file_t f) {
-  if (f.fd != -1) {
-    munmap(f.addr, f.size);
-    close(f.fd);
-  }
-}
-
-char *lookup_name(char *buf, int size, uint16_t VendorId, uint16_t DeviceId) {
-  // Writes into buf, format needs to be consistent
-  // Usually returns buf, sometimes returns a pointer into a string literal
-
-  mmapped_file_t f = open_mmapped_file();
-
+char *pci_ids_lookup(struct pci_ids f, char *buf, size_t size,
+                     uint16_t VendorId, uint16_t DeviceId) {
   if (f.fd == -1) {
     write_fallback_to_buffer(buf, size, DeviceId);
     return buf;
@@ -245,7 +281,6 @@ char *lookup_name(char *buf, int size, uint16_t VendorId, uint16_t DeviceId) {
     write_fallback_to_buffer(buf, size, DeviceId);
   }
 
-  close_mmapped_file(f);
   return buf;
 }
 
@@ -262,7 +297,7 @@ static MODULE(write_as_four_hex) {
     char ref[5];
     char val[4];
     for (uint32_t i = 0; i < UINT16_MAX; i++) {
-      write_as_four_hex_chars(i, val);
+      write_as_hex(i, val);
       CHECK(4 == snprintf(ref, 5, "%04x", i));
       CHECK(memcmp(ref, val, 4) == 0);
     }
@@ -273,12 +308,12 @@ MAIN_MODULE() {
   DEPENDS(write_as_four_hex);
 
   TEST("") {
-    mmapped_file_t file = open_mmapped_file();
+    struct pci_ids file = pci_ids_create();
     CHECK(file.fd != -1);
     if (file.fd != -1) {
       char rbuffer[128] = {0};
       char pbuffer[sizeof(rbuffer)] = {0};
-      int size = sizeof(rbuffer);
+      size_t size = sizeof(rbuffer);
 
       uint16_t VegaDeviceId = 26287;
       uint16_t AMDVendorId = 4098;
@@ -316,6 +351,6 @@ MAIN_MODULE() {
         }
       }
     }
-    close_mmapped_file(file);
+    pci_ids_destroy(file);
   }
 }
