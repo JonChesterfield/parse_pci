48747445f742b491c04124e4785e4bf0596708c0
JonChesterfield <jonathanchesterfield@gmail.com>
2020-03-20 01:16:55 +0000

    more tests

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ CC=`which clang`
 DIS=`which llvm-dis`
 OPT=`which opt`
 
-CFLAG="-g -O2 -Wall -Wextra -Wpedantic -std=c99"
+CFLAG="-g -O0 -Wall -Wextra -Wpedantic -std=c99"
 $CC $CFLAG pci_ids_tested.c -c -emit-llvm -o pci_ids.bc
 $CC $CFLAG regression.c -c -o regression.o
 $OPT -strip-debug pci_ids.bc -S -o pci_ids.ll
 
 $CC pci_ids.bc regression.o -o pci_ids -lpci
 
-time ./pci_ids
+time valgrind --leak-check=no ./pci_ids
--- pci_ids_tested.c
+++ pci_ids_tested.c
@@ -3,6 +3,18 @@
 // Shipping to a repo which doesn't do fine grained testing, but want to test the
 // implementation details
 #include "EvilUnit.h"
+#include <stdlib.h>
+
+bool equal_range(struct range x, struct range y)
+{
+	size_t xsz = x.end - x.start;
+	size_t ysz = y.end - y.start;
+	if (xsz != ysz) {
+		return false;
+	}
+
+	return memcmp(x.start, y.start, xsz) == 0;
+}
 
 static MODULE(write_as_hex)
 {
@@ -18,8 +30,42 @@ static MODULE(write_as_hex)
 	}
 }
 
+struct range make_range(const char *str)
+{
+	assert(str);
+	size_t len = strlen(str);
+	unsigned char *mem = malloc(len);
+	assert(mem);
+	memcpy(mem, str, len); // excludes the trailing nul
+	return (struct range){
+		.start = mem,
+		.end = mem + len,
+	};
+}
+
+static MODULE(trim)
+{
+	TEST("")
+	{
+		CHECK(equal_range(trim_whitespace(make_range("")),
+				  trim_whitespace(make_range(" \n \t "))));
+
+		CHECK(equal_range(trim_whitespace(make_range("leading")),
+				  trim_whitespace(make_range("\vleading"))));
+
+		CHECK(equal_range(trim_whitespace(make_range("trailing")),
+				  trim_whitespace(make_range("trailing\n"))));
+
+		CHECK(equal_range(trim_whitespace(make_range("both")),
+				  trim_whitespace(make_range(" \n\tboth  \r"))));
+	}
+}
+
 MAIN_MODULE()
 {
 	DEPENDS(write_as_hex);
-	DEPENDS(regression);
+	DEPENDS(trim);
+	CHECK(equal_range(make_range(""), make_range("")));
+
+	// DEPENDS(regression);
 }
